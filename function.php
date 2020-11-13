e562bdc250c81749d5b691dc3388af6f95c4a19f
Kashif Javed <karlinj9@gmail.com>
2020-11-13 09:03:53 +0500

    PDO and PDO Refractor Done

--- function.php
+++ function.php
@@ -2,21 +2,6 @@
 
     include 'database/Connection.php';
     include 'database/QueryBuilder.php';
-    include 'task.php';    
-
-    // function dd($animal){
-    //     echo '<pre>';
-    //     die(var_dump($animal));
-    //     echo '</pre>';
-    // }
-
-    // function fetchAllTasks($pdo){
-    //     $statement = $pdo->prepare('select * from todo');
-    //     $statement->execute();
-    //     return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-    // }
-
-    //------------------------------------------- 
     
     $pdo = Connection::make();
 
