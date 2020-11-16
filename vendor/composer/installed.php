000fdf9290d088efd8715a70145254301f6aebe9
Kashif Javed <karlinj9@gmail.com>
2020-11-16 07:04:32 +0500

    composer autoloading

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '977008d114265c7e982525c24d5b3480664dbf11',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '977008d114265c7e982525c24d5b3480664dbf11',
+    ),
+  ),
+);
